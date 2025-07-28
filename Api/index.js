081d5cd97bc7177fd0049a376bc509116276aa9f
Zeezay <aolam.ao@gmail.com>
2025-07-28 10:14:10 +0100

    backend is now working

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,35 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+// Load environment variables from .env file
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+// function to connect to MongoDB
+const connectToDb =  async()=>{
+
+try{   
+     await mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+console.log('✅ Connected to MongoDB');
+
+// setting up a simple Express server
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}); 
+app.listen(3000, ()=>{
+ const app = express();
+    console.log('Server is running on port 3000');
+})
+
+}catch(error){
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Stop the process if DB fails to connect
+  }
+}
+
+connectToDb()
