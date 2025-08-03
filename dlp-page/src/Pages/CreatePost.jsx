ba1c634bc487815158869241b20d0f386b95c32a
Zeezay <aolam.ao@gmail.com>
2025-08-03 22:38:06 +0100

    post functionality complete

--- dlp-page/src/Pages/CreatePost.jsx
+++ dlp-page/src/Pages/CreatePost.jsx
@@ -1,10 +1,11 @@
 import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
 import React, { useState } from 'react';
-import LexicalEditor from '../components/LexiEditor';
 import axios from 'axios';
 import {CircularProgressbar} from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
-
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+import {useNavigate} from 'react-router-dom'
 
 
 
@@ -16,8 +17,9 @@ const CreatePost = () => {
   const [errMessage, setErrMessage] = useState('')
   const [formData, setFormData] = useState({})
   const [uploadProgress, setUploadProgress] = useState('')
-
-
+  const [publisherr, setPublishErr] = useState(null) 
+ 
+  const navigate = useNavigate()
 
 const handleChange = async (e)=>{
  const file = e.target.files[0]
@@ -65,17 +67,43 @@ setUploadProgress(null)
 }
 } 
 
+const handleSubmit = async ( e)=>{
+  e.preventDefault()
+try{
+  const res = await fetch('/api/post/create', {
+    method:'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
 
+  const data = await res.json()
+
+  if(!res.ok){
+    setPublishErr(data.message)
+    return
+  }
+
+
+  if(res.ok){
+    setPublishErr(null)
+    navigate(`/post/${data.slug}`)
+  }
+}catch(error){
+setPublishErr('something went wrong')
+}
+}
 
 
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen '>
         <h1 className='text-center text-3xl my-7 font-semibold'>  CreatePost </h1>
-       <form className='flex flex-col gap-4'>
+       <form className='flex flex-col gap-4'  onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
             <TextInput type='text' placeholder='Title' required id='title'
-            className='flex-1'/>
-            <Select className='min-w-40'>
+            className='flex-1' onChange={(e)=>{setFormData({...formData, title:e.target.value})}} />
+            <Select className='min-w-40' onChange={(e)=>{setFormData({...formData, category:e.target.value})}}>
                 <option value='uncategories'> Select a category </option>
                 <option value='news'> Fees </option>
                 <option value='news'> Course </option>
@@ -113,10 +141,15 @@ setUploadProgress(null)
  <img src={formData.image} alt="" className='w-full h-72 object-cover'  /> 
 }
 
-<LexicalEditor/>
+ <ReactQuill theme="snow" placeholder='Write something' className='h-72 mb-5' onChange={(value)=>{
+  setFormData({...formData, content:value})
+}} />
 
- <Button type='submit'> Submit Post</Button>
+  <Button type='submit' className='mt-5' > Submit Post</Button>
 
+  {
+  publisherr && <Alert color='failure' className='mt-6'> {publisherr}</Alert>
+}
 
 
 
