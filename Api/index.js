f4e9835db31f8a87d7226baf546c88d28b37d326
Zeezay123 <aolam.ao@gmail.com>
2025-08-26 10:12:30 +0100

    cors adjust

--- Api/index.js
+++ Api/index.js
@@ -30,14 +30,7 @@ const app = express();
 app.use(express.json()); 
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [
-          process.env.FRONTEND_URL,
-    ],
-    credentials: true, // only if you’re using cookies/auth
-  })
-);
+app.use(cors());
 
 
 
